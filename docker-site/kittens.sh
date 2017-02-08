6833a191ec8949a09208f4c212a95e9452668ad2
ajhaydock <alex@alexhaydock.co.uk>
2017-02-08 17:19:13 +0000

    Download Tor exit list when starting container

--- docker-site/kittens.sh
+++ docker-site/kittens.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-php-fpm && nginx -g "daemon off;"
+
+# Download list of Tor exits
+TOREXITLIST=`curl -s https://check.torproject.org/exit-addresses | grep ExitAddress | awk '{print "\t" $2 " 1;"}'`
+
+# Pipe exit list into nginx-compatible conf file
+echo -e "geo \$torExit {
+\tdefault 0;
+$TOREXITLIST
+}" > /etc/nginx/torexits.conf
+
+# Only load PHP/nginx if the Tor exit file has been properly generated
+if [ -f "/etc/nginx/torexits.conf" ]; then
+	php-fpm && nginx -g "daemon off;"
+else
+	echo "Tor exit list was not downloaded!"
+	echo "Exiting."
+	exit 10
+fi
